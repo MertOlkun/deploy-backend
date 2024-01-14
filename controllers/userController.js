7ce6207efbb990488aa8cdb91bb7ab8fbc777579
MertOlkun <mert_olkun_26@outlook.com>
2024-01-14 19:08:12 +0300

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -110,9 +110,9 @@ async function forgotPassword(req, res) {
         });
       } else {
         console.log("Email sent: " + info.response);
+        res.json({ message: "Login successful", resetToken });
         return res.status(200).json({
           message: `Password reset link has been sent.`,
-          resetToken,
         });
       }
     });
